3bf30c9e1251f76c5cb7f6945e401f109b23a5d2
Mizukami Kien <51937839+kiipoyon@users.noreply.github.com>
2020-12-27 15:31:22 +0900

    Add files via upload

---
+++ manager/kanri_eiga_henshu_fin.php
@@ -0,0 +1,88 @@
+<?php
+    
+	require '../common/common.php';
+	// データベースに接続する
+	$pdo = connect();
+			
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- load css -->
+    <link rel="stylesheet" href="../css/common/reset.css">
+
+    <!-- bootstrap css -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css" integrity="sha384-VCmXjywReHh4PwowAiWNagnWcLhlEJLA5buUprzK8rxFgeH0kww/aWY76TfkUoSX" crossorigin="anonymous">    <!-- bootstrap css end -->
+    <!-- bootstrap css end -->
+
+    <link rel="stylesheet" href="../css/common/common.css"/>
+
+    <link rel="stylesheet" href="../css/common/kanri_common.css">
+    <link rel="stylesheet" href="../css/kanri_eiga_list.css">
+    <link rel="stylesheet" href="../css/m_kanri_henshu.css">
+    <!--load css end-->
+
+    <title>映画情報管理 || HALシネマ</title>
+</head>
+<body>
+<!-- header -->
+<header id="header">
+
+    <!-- logo -->
+    <div>
+        <img src="../images/common/logo.png" alt="HALcinema-logo">
+    </div>
+    <!-- logo end -->
+
+</header>
+<!-- header end -->
+
+<!-- nav -->
+<nav>
+
+    <ul>
+        <li>
+            <a href="kanri_top.php">
+                <h3 id="top">管理者トップ</h3>
+            </a>
+        </li>
+        <li>
+            <a href="kanri_uriage.php">
+                <h3 id="uriage">売上情報管理</h3>
+            </a>
+        </li>
+        <li>
+            <a href="kanri_eiga.php">
+                <h3 id="eiga">映画情報管理</h3>
+            </a>
+        </li>
+        <li>
+            <a href="kanri_kaiin.php">
+                <h3 id="kaiin">会員情報管理</h3>
+            </a>
+        </li>
+        <li>
+            <a href="kanri_zaseki.php">
+                <h3 id="zaseki">座席予約管理</h3>
+            </a>
+        </li>
+    </ul>
+
+</nav>
+
+<h1>映画　管理</h1>
+
+<form method="post">
+	<div>
+        <p>更新が完了しました</p>
+	</div>
+
+	<a href="kanri_eiga.php">完了</a>
+	<input type="hidden" name="message_id" value="<?php echo $message_data['movie_no']; ?>">
+</form>
+
+</body>
+</html>
