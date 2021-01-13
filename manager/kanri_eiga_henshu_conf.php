24d85114dde436f2f199bb7092663ee8dee47ca6
k-kashi <65828267+k-kashi@users.noreply.github.com>
2021-01-14 00:39:51 +0900

    cssによるclass追加

--- manager/kanri_eiga_henshu_conf.php
+++ manager/kanri_eiga_henshu_conf.php
@@ -36,6 +36,7 @@
 ?>
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -44,99 +45,103 @@
     <link rel="stylesheet" href="../css/common/reset.css">
 
     <!-- bootstrap css -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css" integrity="sha384-VCmXjywReHh4PwowAiWNagnWcLhlEJLA5buUprzK8rxFgeH0kww/aWY76TfkUoSX" crossorigin="anonymous">    <!-- bootstrap css end -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css"
+        integrity="sha384-VCmXjywReHh4PwowAiWNagnWcLhlEJLA5buUprzK8rxFgeH0kww/aWY76TfkUoSX" crossorigin="anonymous">
+    <!-- bootstrap css end -->
     <!-- bootstrap css end -->
 
-    <link rel="stylesheet" href="../css/common/common.css"/>
+    <link rel="stylesheet" href="../css/common/common.css" />
 
     <link rel="stylesheet" href="../css/common/kanri_common.css">
-		<link rel="stylesheet" href="../css/kanri_eiga_list.css">
-		<link rel="stylesheet" href="../css/kanri_eiga.css">
+    <link rel="stylesheet" href="../css/kanri_eiga_list.css">
+    <link rel="stylesheet" href="../css/kanri_eiga.css">
     <link rel="stylesheet" href="../css/kanri_eiga_henshu.css">
     <!--load css end-->
 
     <title>映画情報管理 || HALシネマ</title>
 </head>
+
 <body>
-<!-- header -->
-<header id="header">
+    <!-- header -->
+    <header id="header">
 
-    <!-- logo -->
-    <div>
-        <img src="../images/common/logo.png" alt="HALcinema-logo">
-    </div>
-    <!-- logo end -->
+        <!-- logo -->
+        <div>
+            <img src="../images/common/logo.png" alt="HALcinema-logo">
+        </div>
+        <!-- logo end -->
 
-</header>
-<!-- header end -->
+    </header>
+    <!-- header end -->
 
-<!-- nav -->
-<nav>
+    <!-- nav -->
+    <nav>
 
-    <ul>
-        <li>
-            <a href="kanri_top.php">
-                <h3 id="top">管理者トップ</h3>
-            </a>
-        </li>
-        <li>
-            <a href="kanri_uriage.php">
-                <h3 id="uriage">売上情報管理</h3>
-            </a>
-        </li>
-        <li>
-            <a href="kanri_eiga.php">
-                <h3 id="eiga">映画情報管理</h3>
-            </a>
-        </li>
-        <li>
-            <a href="kanri_kaiin.php">
-                <h3 id="kaiin">会員情報管理</h3>
-            </a>
-        </li>
-        <li>
-            <a href="kanri_zaseki.php">
-                <h3 id="zaseki">座席予約管理</h3>
-            </a>
-        </li>
-    </ul>
+        <ul>
+            <li>
+                <a href="kanri_top.php">
+                    <h3 id="top">管理者トップ</h3>
+                </a>
+            </li>
+            <li>
+                <a href="kanri_uriage.php">
+                    <h3 id="uriage">売上情報管理</h3>
+                </a>
+            </li>
+            <li>
+                <a href="kanri_eiga.php">
+                    <h3 id="eiga">映画情報管理</h3>
+                </a>
+            </li>
+            <li>
+                <a href="kanri_kaiin.php">
+                    <h3 id="kaiin">会員情報管理</h3>
+                </a>
+            </li>
+            <li>
+                <a href="kanri_zaseki.php">
+                    <h3 id="zaseki">座席予約管理</h3>
+                </a>
+            </li>
+        </ul>
 
-</nav>
+    </nav>
 
-<main class="main_wrap">
+    <main class="main_wrap">
 
-	<h4>映画情報管理　編集</h4>
+        <h4>映画情報管理　編集</h4>
 
-	<form method="post">
-		<div>
-			<label for="title">タイトル</label>
-			<?php echo $title; ?>
-		</div>
-		<div>
-			<label for="image">サムネイル</label>
-			<?php echo $image; ?>
-		</div>
-		<div>
-			<label for="story">あらすじ</label>
-			<?php echo $story; ?>
-		</div>
-		<div>
-			<label for="director">監督名　</label>
-			<?php echo $director; ?>
-		</div>
-		<div>
-			<label for="cast">キャスト</label>
-			<?php echo $cast; ?>
-		</div>
-		<div class="btn">
-			<input type="button" onclick="history.back()" value="戻る">
-			<input type="submit" name="apdate_btn" value="更新">
-		</div>
+        <form method="post" class="conf_main">
+            <div class="conf_div">
+                <label for="title">タイトル</label>
+                <p><?php echo $title; ?></p>
+            </div>
+            <div class="conf_div">
+                <label for="image">サムネイル</label>
+                <p><?php echo $image; ?></p>
+            </div>
+            <div class="conf_div">
+                <label for="story">あらすじ</label>
+                <p><?php echo $story; ?></p>
+            </div>
+            <div class="conf_div">
+                <label for="director">監督名　</label>
+                <p><?php echo $director; ?></p>
+            </div>
+            <div class="conf_div">
+                <label for="cast">キャスト</label>
+                <p><?php echo $cast; ?></p>
+            </div>
+            <div class="btn">
+                <input type="button" onclick="history.back()" value="戻る">
+                <input type="submit" name="apdate_btn" value="更新">
+            </div>
 
-		<input type="hidden" name="message_id" value="<?php echo $message_data['movie_no']; ?>">
-	</form>
+            <input type="hidden" name="message_id" value="<?php echo $message_data['movie_no']; ?>">
+        </form>
 
-</main>
+    </main>
 
 </body>
+
 </html>
--- manager/kanri_eiga_henshu_fin.php
+++ manager/kanri_eiga_henshu_fin.php
@@ -7,6 +7,7 @@
 ?>
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -15,10 +16,12 @@
     <link rel="stylesheet" href="../css/common/reset.css">
 
     <!-- bootstrap css -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css" integrity="sha384-VCmXjywReHh4PwowAiWNagnWcLhlEJLA5buUprzK8rxFgeH0kww/aWY76TfkUoSX" crossorigin="anonymous">    <!-- bootstrap css end -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/css/bootstrap.min.css"
+        integrity="sha384-VCmXjywReHh4PwowAiWNagnWcLhlEJLA5buUprzK8rxFgeH0kww/aWY76TfkUoSX" crossorigin="anonymous">
+    <!-- bootstrap css end -->
     <!-- bootstrap css end -->
 
-    <link rel="stylesheet" href="../css/common/common.css"/>
+    <link rel="stylesheet" href="../css/common/common.css" />
 
     <link rel="stylesheet" href="../css/common/kanri_common.css">
     <link rel="stylesheet" href="../css/kanri_eiga_list.css">
@@ -28,66 +31,68 @@
 
     <title>映画情報管理 || HALシネマ</title>
 </head>
+
 <body>
-<!-- header -->
-<header id="header">
+    <!-- header -->
+    <header id="header">
 
-    <!-- logo -->
-    <div>
-        <img src="../images/common/logo.png" alt="HALcinema-logo">
-    </div>
-    <!-- logo end -->
-
-</header>
-<!-- header end -->
-
-<!-- nav -->
-<nav>
-
-    <ul>
-        <li>
-            <a href="kanri_top.php">
-                <h3 id="top">管理者トップ</h3>
-            </a>
-        </li>
-        <li>
-            <a href="kanri_uriage.php">
-                <h3 id="uriage">売上情報管理</h3>
-            </a>
-        </li>
-        <li>
-            <a href="kanri_eiga.php">
-                <h3 id="eiga">映画情報管理</h3>
-            </a>
-        </li>
-        <li>
-            <a href="kanri_kaiin.php">
-                <h3 id="kaiin">会員情報管理</h3>
-            </a>
-        </li>
-        <li>
-            <a href="kanri_zaseki.php">
-                <h3 id="zaseki">座席予約管理</h3>
-            </a>
-        </li>
-    </ul>
-
-</nav>
-
-<main class="main_wrap">
-
-    <h4>映画情報管理　編集</h4>
-
-    <form method="post">
+        <!-- logo -->
         <div>
-            <p>更新が完了しました</p>
+            <img src="../images/common/logo.png" alt="HALcinema-logo">
         </div>
+        <!-- logo end -->
 
-        <a href="kanri_eiga.php">完了</a>
-        <input type="hidden" name="message_id" value="<?php echo $message_data['movie_no']; ?>">
-    </form>
+    </header>
+    <!-- header end -->
 
-</main>
+    <!-- nav -->
+    <nav>
+
+        <ul>
+            <li>
+                <a href="kanri_top.php">
+                    <h3 id="top">管理者トップ</h3>
+                </a>
+            </li>
+            <li>
+                <a href="kanri_uriage.php">
+                    <h3 id="uriage">売上情報管理</h3>
+                </a>
+            </li>
+            <li>
+                <a href="kanri_eiga.php">
+                    <h3 id="eiga">映画情報管理</h3>
+                </a>
+            </li>
+            <li>
+                <a href="kanri_kaiin.php">
+                    <h3 id="kaiin">会員情報管理</h3>
+                </a>
+            </li>
+            <li>
+                <a href="kanri_zaseki.php">
+                    <h3 id="zaseki">座席予約管理</h3>
+                </a>
+            </li>
+        </ul>
+
+    </nav>
+
+    <main class="main_wrap">
+
+        <h4>映画情報管理　編集</h4>
+
+        <form method="post" class="fin_main">
+            <div class="fin_div">
+                <p>更新が完了しました</p>
+            </div>
+
+            <a href="kanri_eiga.php">映画情報管理へ戻る</a>
+            <input type="hidden" name="message_id" value="<?php echo $message_data['movie_no']; ?>">
+        </form>
+
+    </main>
 
 </body>
+
 </html>
