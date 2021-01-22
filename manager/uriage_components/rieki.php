645b908044a556488dd0f6381d26b8887eba524b
nhsKK <lightsiber66@gmail.com>
2021-01-22 16:21:05 +0900

    test

--- manager/uriage_components/rieki.php
+++ manager/uriage_components/rieki.php
@@ -27,11 +27,13 @@
         
         // 各月売上総額計算
         $sql = 'SELECT * FROM sale_tbl WHERE sale_date>=' . $year . $i . '01 AND sale_date<=' . $year . $i . '31';
-        $stmt = $dbh->query($sql);
+        $sales = $dbh->query($sql);
             
-        foreach ($stmt as $row){
-            $uriage =+ $row;
-        }
+        $uriage = array_sum($sales);
+        
+        // foreach ($stmt as $row){
+        //     $uriage =+ $row;
+        // }
         
         // 各月経費総額計算
         // $sql = 'SELECT * FROM cost_tbl WHERE cost_date>=' . $year . $i . '01 AND cost_date<=' . $year . $i . '31';
